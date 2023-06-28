f98d904aa097eae0b95d5730f2dbc5324dd4ce11
furkankaan <fkbicer07@gmail.com>
2023-06-29 01:06:18 +0300

    01.05 - 01.06 added

---
+++ 01-spring-boot-overview-r/06-properties-demo/src/main/java/com/example/demo/rest/FunRestController.java
@@ -0,0 +1,39 @@
+package com.example.demo.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // inject properties for: coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+     // expose new endpoint for "teaminfo"
+
+    @GetMapping("/teaminfo")
+    public String teamInfo(){
+        return "Coach Name: " + coachName + " Team Name: " + teamName;
+    }
+
+    // expose '/'  that return 'Hello World'
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World";
+    }
+
+    // expose a new endpoint for "workout",
+
+    @GetMapping("/workout")
+        public String getDailyWorkout() {
+            return "Run a hard 5k as";
+        }
+
+    
+}
