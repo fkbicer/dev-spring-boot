54555c7bdcad109219fe371637bc89814d050cf1
furkankaan <fkbicer07@gmail.com>
2023-07-17 13:40:56 +0300

    path variable added

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/example/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/example/demo/rest/StudentRestController.java
@@ -4,26 +4,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entitiy.Student;
 
+import jakarta.annotation.PostConstruct;
+
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+
+    private List<Student> theStudents; 
+ 
+    @PostConstruct
+    public void loadData(){
+        theStudents = new ArrayList<>();
+        theStudents.add(new Student("Furkan","Kaan"));
+        theStudents.add(new Student("Ahmet","Bilge"));
+        theStudents.add(new Student("Ayten","Sena"));
+        theStudents.add(new Student("Gulten","Suha"));
+    }
     
     // define endpoint for "/students" - return a list of students
 
     @GetMapping("/students")
     public List<Student> getStudents() {
-        List<Student> theStudents = new ArrayList<>();
-        
-        theStudents.add(new Student("Furkan","Kaan"));
-        theStudents.add(new Student("Ahmet","Bilge"));
-        theStudents.add(new Student("Ayten","Sena"));
-        theStudents.add(new Student("Gulten","Suha"));
-
+       
         return theStudents;
     }
+
+    // define endpoint for "/students/{studentId}" - return single student at index
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return theStudents.get(studentId);
+    }
+
 }
