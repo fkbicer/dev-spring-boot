c1b92ddc398b1b5b28bc5c33d2b160032631d4d4
furkankaan <fkbicer07@gmail.com>
2023-07-22 23:39:00 +0300

    saving form data

--- 08-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 08-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -42,6 +44,17 @@ public class EmployeeController {
 		theModel.addAttribute("employee", theEmployee);
 
 		return "employees/employee-form";
+	}
+
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+
+		// save the employee
+		employeeService.save(theEmployee);
+
+		//use a redirect to prevent duplicate submissions
+		return "redirect:/employees/list";
+
 	}
 }
 
