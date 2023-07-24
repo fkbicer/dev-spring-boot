1716aa784fbef0fd62ce9e94700f869d9337e57a
furkankaan <fkbicer07@gmail.com>
2023-07-24 19:15:33 +0300

    custom login form html file

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/example/springboot/demosecurity/controller/LoginController.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/example/springboot/demosecurity/controller/LoginController.java
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class DemoController {
-    
-    @GetMapping("/")
-    public String home(){
+public class LoginController {
 
-        return "home";
+    @GetMapping("/showMyLoginPage")
+    public String showMyLoginPage(){
+
+        return "plain-login";
     }
     
 }
