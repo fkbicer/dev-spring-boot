cc730325e02c15e57a83b5be0c8ff4d72581353f
furkankaan <fkbicer07@gmail.com>
2023-07-27 14:18:02 +0300

    added leadership html and href

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/example/springboot/demosecurity/controller/DemoController.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/example/springboot/demosecurity/controller/DemoController.java
@@ -11,5 +11,13 @@ public class DemoController {
 
         return "home";
     }
+
+    // add a request mapping for /leaders
+
+     @GetMapping("/leaders")
+    public String showLeaders(){
+
+        return "leaders";
+    }
     
 }
