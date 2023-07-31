ab2c44a7729f3e725b5df481785643847f5880ce
furkankaan <fkbicer07@gmail.com>
2023-07-31 20:55:19 +0300

    one to many init

---
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/example/cruddemo/CruddemoApplication.java
@@ -0,0 +1,109 @@
+package com.example.cruddemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.cruddemo.dao.AppDAO;
+import com.example.cruddemo.entity.Instructor;
+import com.example.cruddemo.entity.InstructorDetail;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	
+	@Bean
+	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
+		return runner -> {
+			// createInstructor(appDAO);
+
+			// findInstructorById(appDAO);
+
+			// deleteInstructor(appDAO);
+
+			// findInstructorDetail(appDAO);
+
+			deleteInstructorDetail(appDAO);
+		};
+	}
+
+
+	private void deleteInstructorDetail(AppDAO appDAO) {
+		int theId=4;
+		System.out.println("Id:4 is deleting...");
+		appDAO.deleteInstructorDetailById(4);
+		System.out.println("Done!");
+
+
+	}
+
+
+	private void findInstructorDetail(AppDAO appDAO) {
+			int theId = 2;
+			InstructorDetail tempInstructorDetail = appDAO.findInstructorDetailById(theId);
+
+			System.out.println("tempInstructorDetail: " + tempInstructorDetail);
+
+			System.out.println("the associated Instructor: " + tempInstructorDetail.getInstructor());
+
+			System.out.println("done!");
+
+	}
+
+
+	private void deleteInstructor(AppDAO appDAO) {
+		int theId=1;
+		System.out.println("Deleting instructor id: " + theId);
+
+		appDAO.deleteInstructorById(theId);
+		System.out.println("done!");
+
+	}
+
+
+	private void findInstructorById(AppDAO appDAO) {
+		int theId=1;
+		System.out.println("Finding instructor id: " + theId);
+
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+
+		System.out.println("tempInstructor: " + tempInstructor);
+		System.out.println("the associate instructorDetail only" + tempInstructor.getInstructorDetail());
+
+	}
+
+
+	public void createInstructor(AppDAO appDAO) {
+
+		// create the instructor
+
+		Instructor tempInstructor =
+					new Instructor("Chad","Darby", "darby@blabla.com");
+
+		// create the instructor detail
+
+		InstructorDetail tempInstructorDetail =
+					new InstructorDetail("http://www.clacla.com/youtube", "coding is my hobby");
+
+		
+		// associate the objects
+
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		// save the instructor.
+		System.out.println("Saving instructor: " + tempInstructor);
+		appDAO.save(tempInstructor);
+
+
+					
+
+	}
+
+
+
+}
---
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/example/cruddemo/dao/AppDAOImpl.java
@@ -0,0 +1,68 @@
+package com.example.cruddemo.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.cruddemo.entity.Instructor;
+import com.example.cruddemo.entity.InstructorDetail;
+
+import jakarta.persistence.EntityManager;
+
+@Repository
+public class AppDAOImpl implements AppDAO {
+
+    // define field for entity manager
+
+    private EntityManager entityManager;
+
+    // inject entity manager using contr inj.
+
+    @Autowired
+    public AppDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+
+    @Override
+    @Transactional
+    public void save(Instructor theInsturctor) {
+        
+        entityManager.persist(theInsturctor);
+
+    }
+
+
+    @Override
+    public Instructor findInstructorById(int theId) {
+            return entityManager.find(Instructor.class, theId);
+    }
+
+
+    @Override
+    @Transactional
+    public void deleteInstructorById(int theId) {
+        Instructor tempInstructor = entityManager.find(Instructor.class,theId);
+        entityManager.remove(tempInstructor);
+    }
+
+
+    @Override
+    public InstructorDetail findInstructorDetailById(int theId) {
+     return entityManager.find(InstructorDetail.class, theId);
+    }
+
+
+    @Override
+    @Transactional
+    public void deleteInstructorDetailById(int theId) {
+        InstructorDetail tempInstructorDetail = entityManager.find(InstructorDetail.class,theId);
+
+        // remove associated object referemce
+        // breake bi-directional link.
+        tempInstructorDetail.getInstructor().setInstructorDetail(null);
+
+        entityManager.remove(tempInstructorDetail);
+    }
+    
+}
