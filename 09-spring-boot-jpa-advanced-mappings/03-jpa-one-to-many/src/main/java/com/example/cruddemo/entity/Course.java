b556d370efe0171bd3c718634a49d5078b6ec366
furkankaan <fkbicer07@gmail.com>
2023-07-31 20:57:56 +0300

    course entity impl

--- 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/example/cruddemo/entity/Course.java
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/example/cruddemo/entity/Course.java
@@ -1,6 +1,13 @@
 package com.example.cruddemo.entity;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -11,11 +18,16 @@ public class Course {
     public String toString() {
         return "Course [id=" + id + ", title=" + title + "]";
     }
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private int id;
 
+    @Column(name="title")
     private String title;
 
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name="instructor_id")
     private Instructor instructor;
 
     public Course() {
