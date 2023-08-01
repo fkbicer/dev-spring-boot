13de9870233ad73593efae5be5499e678081720c
furkankaan <fkbicer07@gmail.com>
2023-08-01 13:35:53 +0300

    bidirectional connection between inst and course

--- 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/example/cruddemo/entity/Instructor.java
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/example/cruddemo/entity/Instructor.java
@@ -1,5 +1,8 @@
 package com.example.cruddemo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -7,6 +10,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
@@ -43,6 +47,9 @@ public class Instructor {
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name="instructor_detail_id")
     private InstructorDetail instructorDetail;
+    
+    @OneToMany(mappedBy = "instructor", cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    private List<Course> courses;
 
      public Instructor() {
 
@@ -95,6 +102,16 @@ public class Instructor {
         this.email = email;
     }
 
+     public List<Course> getCourses() {
+        return courses;
+    }
+
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+
 
     public InstructorDetail getInstructorDetail() {
         return instructorDetail;
@@ -111,6 +128,21 @@ public class Instructor {
         return "Instructor [Id=" + Id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
                 + ", instructorDetail=" + instructorDetail + "]";
     }
+
+    // add convenience methods for bi-directional relationship
+
+    public void add(Course tempCourse) {
+
+        if (courses == null) {
+            courses = new ArrayList<>();
+        }
+
+        courses.add(tempCourse);
+
+        tempCourse.setInstructor(this);
+
+    }
+
 
    
     
