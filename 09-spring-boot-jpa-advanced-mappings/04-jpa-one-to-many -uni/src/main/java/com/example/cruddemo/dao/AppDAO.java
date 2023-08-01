777b60d3d4a8fe82f8b16ca0330d47880cd26d2a
furkankaan <fkbicer07@gmail.com>
2023-08-01 18:55:29 +0300

    one-to-many-uni init

---
+++ 09-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many -uni/src/main/java/com/example/cruddemo/dao/AppDAO.java
@@ -0,0 +1,33 @@
+package com.example.cruddemo.dao;
+
+import java.util.List;
+
+import com.example.cruddemo.entity.Course;
+import com.example.cruddemo.entity.Instructor;
+import com.example.cruddemo.entity.InstructorDetail;
+
+public interface AppDAO {
+
+    void save(Instructor theInsturctor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+
+    InstructorDetail findInstructorDetailById(int theId);
+
+    void deleteInstructorDetailById(int theId);
+
+    List<Course> findCoursesByInstructorId(int theId);
+
+    Instructor findInstructorByIdJoinFetch(int theId);
+
+    void update(Instructor instructor);
+
+    void update(Course course);
+
+    Course findCourseById(int theId);
+
+    void deleteCourseById(int theId);
+    
+}
---
+++ 09-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many -uni/src/main/java/com/example/cruddemo/entity/Instructor.java
@@ -0,0 +1,153 @@
+package com.example.cruddemo.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="instructor")
+public class Instructor {
+
+    // annotate the calss as an entity and map to db table
+
+    // define the fields
+
+    // annotate the fileds with db column names
+
+    // create constructors
+
+    // generate getter/setter methods
+
+    // generate toSring() method.
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int Id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="instructor_detail_id")
+    private InstructorDetail instructorDetail;
+    
+    @OneToMany(mappedBy = "instructor",fetch = FetchType.LAZY ,cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    private List<Course> courses;
+
+     public Instructor() {
+
+    }
+
+
+    public Instructor(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+
+    public int getId() {
+        return Id;
+    }
+
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+     public List<Course> getCourses() {
+        return courses;
+    }
+
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+
+
+    public InstructorDetail getInstructorDetail() {
+        return instructorDetail;
+    }
+
+
+    public void setInstructorDetail(InstructorDetail instructorDetail) {
+        this.instructorDetail = instructorDetail;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Instructor [Id=" + Id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+                + ", instructorDetail=" + instructorDetail + "]";
+    }
+
+    // add convenience methods for bi-directional relationship
+
+    public void add(Course tempCourse) {
+
+        if (courses == null) {
+            courses = new ArrayList<>();
+        }
+
+        courses.add(tempCourse);
+
+        tempCourse.setInstructor(this);
+
+    }
+
+
+   
+    
+
+
+    
+}
