716122c6e6952b9f2be9fe101354496ce4a27843
furkankaan <fkbicer07@gmail.com>
2023-08-02 03:05:55 +0300

    student link w course

--- 09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/example/cruddemo/entity/Student.java
+++ 09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/example/cruddemo/entity/Student.java
@@ -1,10 +1,18 @@
 package com.example.cruddemo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -24,6 +32,18 @@ public class Student {
 
     @Column(name="email")
     private String email;
+
+    @ManyToMany(fetch = FetchType.LAZY ,cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(name = "course_student", joinColumns = @JoinColumn(name="student_id"),
+    inverseJoinColumns = @JoinColumn(name="course_id"))
+    private List<Course> courses;
+
+    public void addCourse(Course theCourse) {
+        if (courses == null) {
+            courses = new ArrayList<>();
+        }
+        courses.add(theCourse);
+    }
 
      public Student() {
         
@@ -60,6 +80,12 @@ public class Student {
     @Override
     public String toString() {
         return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+    }
+    public List<Course> getCourses() {
+        return courses;
+    }
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
     }
     
     
