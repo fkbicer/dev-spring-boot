35717f7aef5a859919aec5c558335686b24d3a63
furkankaan <fkbicer07@gmail.com>
2023-08-03 17:08:56 +0300

    add more advice

--- 10-spring-boot-aop/02-spring-boot-aop-demo -pointcut-declarations/src/main/java/com/example/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo -pointcut-declarations/src/main/java/com/example/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -20,8 +20,11 @@ public class MyDemoLoggingAspect {
 
     @Before("forDaoPackage()")
     public void beforeAddAccountAdvice(){
-     
         System.out.println("\n========> Executing @Before advice on method.");
+    }
 
+    @Before("forDaoPackage()")
+    public void performApiAnalytics() {
+         System.out.println("\n========> Performing API analytics.");
     }
 }
