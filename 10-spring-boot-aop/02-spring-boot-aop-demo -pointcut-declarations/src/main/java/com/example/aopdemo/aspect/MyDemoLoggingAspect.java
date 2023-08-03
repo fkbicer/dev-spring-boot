2cfc2dec3b11276e4bd711db71e6e28683035289
furkankaan <fkbicer07@gmail.com>
2023-08-03 18:08:25 +0300

    added method

--- 10-spring-boot-aop/02-spring-boot-aop-demo -pointcut-declarations/src/main/java/com/example/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo -pointcut-declarations/src/main/java/com/example/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,7 +1,9 @@
 package com.example.aopdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +13,17 @@ import org.springframework.stereotype.Component;
 public class MyDemoLoggingAspect {
 
     @Before("com.example.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterAndSetter()")
-    public void beforeAddAccountAdvice(){
+    public void beforeAddAccountAdvice(JoinPoint theJoinPoint){
         System.out.println("\n========> Executing @Before advice on method.");
+
+        
+    // display the method signature
+    MethodSignature methodSignature = (MethodSignature) theJoinPoint.getSignature();
+    System.out.println("Method : " + methodSignature);
+    // display method arguments
+    
+    
+
     }
+
 }
